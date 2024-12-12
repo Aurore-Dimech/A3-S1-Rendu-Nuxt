ba3d9495f7354bcc16fad53bb887f47ac4712780
Aurore <aurore@Aurores-MacBook-Pro.local>
2024-12-12 13:35:41 +0100

    ADD finished project with bonus

--- composables/useQuiz.js
+++ composables/useQuiz.js
@@ -1,20 +1,32 @@
 export default function(initialQuiz) {
     const quiz = reactive(initialQuiz)
 
-    const count = ref(0)
+    const time_question = ref(Date.now())
 
-    const question = ref(quiz.questions[count.value])
+    const count = ref(0)
 
     const score = ref(0)
 
+    const question = ref(quiz.questions[count.value])
+
     const submitAnswer = (answer) => {
+        const time_answer = Date.now()
         if(answer === question.value.answers.true_answer){
-            score.value++
+            const time_to_respond = time_answer-time_question.value
+
+            if (time_to_respond < 1000){
+                score.value += 5
+            } else if (time_to_respond > 5000){
+                score.value++
+            } else {
+                score.value += Math.round(5-time_to_respond/1000)
+            }
         }
 
         count.value++
 
         question.value = quiz.questions[count.value];
+        time_question.value = Date.now();
     }
 
     const resetQuiz = () => {
